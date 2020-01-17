e8b4fb05c22680dd6a8bc0d52118248caa99e23e
Artem <thegreatartem+git@gmail.com>
2020-01-17 16:58:51 +0800

    finished seed check

--- ivseedcheck.js
+++ ivseedcheck.js
@@ -1,158 +1,145 @@
-        function SetCheckResult(result){
-			let text = ''
-			let color = ''
-			switch (result)
-			{
-				case -1:
-					text = "-";
-					color = "black";
-					break;
+function SetCheckResult(result) {
+    let text = '';
+    let color = '';
+    switch (result) {
+        case -1:
+            text = "-";
+            color = "black";
+            break;
 
-				case 1:
-					text = "No Good!";
-					color = 'red';
-					break;
+        case 1:
+            text = "No Good!";
+            color = 'red';
+            break;
 
-				case 3:
-				    text = "OK! Next -> 3IV";
-					color = 'blue';
-					break;
+        case 3:
+            text = "OK! Next -> 3IV";
+            color = 'blue';
+            break;
 
-				case 4:
-					text =	"OK! Next -> 4IV",
-					color = 'blue';
-					break;
+        case 4:
+            text = "OK! Next -> 4IV";
+                color = 'blue';
+            break;
 
-				case 7:
-					text = "OK! Next -> 3IV or 4IV",
-					color = 'blue';
-					break;
-			}
-			return [text,color]
-		}
+        case 7:
+            text = "OK! Next -> 3IV or 4IV";
+                color = 'blue';
+            break;
+    }
+    return [text, color]
+}
 
-		// 個体値チェックボタン
-function checkIvs(object sender, EventArgs e)
-		{
-			let isCheckFailed = false;
-			let errorText = "";
+// 個体値チェックボタン
 
-			// フォームから必要な情報を取得
-			let ivs = Array(6);
-			for (int i = 0; i < 6; i++)
-			{
-				try
-				{
-					ivs[i] = int.Parse(m_PokemonInfo[2].TextBoxIvs[i].Text);
-				}
-				catch (Exception)
-				{
-					// エラー
-					errorText = Messages.Instance.ErrorMessage["IvsFormat"];
-					isCheckFailed = true;
-				}
-				if (ivs[i] < 0 || ivs[i] > 31)
-				{
-					// エラー
-					errorText = Messages.Instance.ErrorMessage["IvsFormat"];
-					isCheckFailed = true;
-				}
-			}
-			if (isCheckFailed)
-			{
-				CreateErrorDialog(errorText);
-				return;
-			}
+let testivs = [31,16,31,24,23,3];
 
-			// 1匹目はVが2or3箇所じゃないとエラー
-			int strict = (Get35Mode() == Star35PanelMode.From3V ? 3 : 2);
-			int c = 0;
-			for (int i = 0; i < 6; ++i)
-			{
-				if (ivs[i] == 31)
-				{
-					++c;
-				}
-			}
-			if (c != strict)
-			{
-				// エラー
-				if (strict == 3)
-				{
-					CreateErrorDialog(Messages.Instance.ErrorMessage["IvsStrict351_3V"]);
-					return;
-				}
-				else
-				{
-					CreateErrorDialog(Messages.Instance.ErrorMessage["IvsStrict351_2V"]);
-					return;
-				}
-			}
+function checkIvs(inputivs) {
+    let isCheckFailed = false;
+    let errorText = "";
+    let is3iv = false;
+    // フォームから必要な情報を取得
+    let ivs = Array(6);
+    for (let i = 0; i < 6; i++) {
+        try {
+            ivs[i] = parseInt(inputivs[i]);
+        }
+        catch (Exception) {
+            // エラー
+            errorText = 'Ivs not a number';
+            isCheckFailed = true;
+        }
+        if (ivs[i] < 0 || ivs[i] > 31) {
+            // エラー
+            errorText = 'Ivs outside range of 0-31';
+            isCheckFailed = true;
+        }
+    }
+    if (isCheckFailed) {
+        alert(errorText);
+        return;
+    }
 
-			// 遺伝箇所チェック
-			bool[] possible = { false, false, false, false, false };
-			bool[] vFlag = { false, false, false, false, false, false };
-			int fixedCount = 0;
-			for (int i = 0; i < 6; ++i)
-			{
-				if (ivs[i] == 31)
-				{
-					vFlag[i] = true;
-					++fixedCount;
-				}
-			}
+    // 1匹目はVが2or3箇所じゃないとエラー
+    let ivtype = inputivs.reduce((a,b)=>{return a+((b===31)?1:0)},0);
+    if(ivtype>3 || ivtype<2){
+        alert('must be 2 or 3 iv');
+        return SetCheckResult(1);
+    }
+    if(ivtype===3){
+        is3iv = true;
+    }
+    let strict = ivtype;
+    let c = 0;
+    for (let i = 0; i < 6; i++) {
+        if (ivs[i] === 31) {
+            c++;
+        }
+    }
+    if (c !== strict) {
+        // エラー
+        if (strict === 3) {
+            alert('must be 3iv');
+            return;
+        }
+        else {
+            alert('must be 2iv');
+            return;
+        }
+    }
 
-			// 2V+3Vor4Vで6個確定
-			// 3V+4Vで5個確定
-			int needNumber = (Get35Mode() == Star35PanelMode.From2V ? 6 : 5);
+    // 遺伝箇所チェック
+    let possible = [false, false, false, false, false];
+    let vFlag = [false, false, false, false, false, false];
+    let fixedCount = 0;
+    for (let i = 0; i < 6; i++) {
+        if (ivs[i] === 31) {
+            vFlag[i] = true;
+            fixedCount++;
+        }
+    }
 
-			for (int vCount = fixedCount + 1; vCount <= 4; ++vCount)
-			{
-				c = fixedCount;
-				int used = fixedCount;
-				for (int i = 0; i < 6; ++i)
-				{
-					vFlag[i] = (ivs[i] == 31);
-				}
-				// 普通に個体値生成をやってみる
-				for (int i = 0; i < 6; ++i)
-				{
-					if (ivs[i] != 31)
-					{
-						++used;
-						int vPos = ivs[i] % 8;
-						if (vPos < 6 && vFlag[vPos] == false)
-						{
-							vFlag[vPos] = true;
-							++c;
-							if (c == vCount) // 遺伝終わり
-							{
-								// 未知の部分が連続してneedNumber以上
-								if ((6 - vCount) - (6 - used) + (6 - fixedCount) >= needNumber)
-								{
-									possible[vCount] = true;
-								}
-								break;
-							}
-						}
-					}
-				}
-			}
+    // 2V+3Vor4Vで6個確定
+    // 3V+4Vで5個確定
+    let needNumber = (!is3iv/* im assuming this means 2iv panel Get35Mode() === Star35PanelMode.From2V*/ ? 6 : 5);
 
-			if (possible[3] && possible[4])
-			{
-				SetCheckResult(7);
-			}
-			else if (possible[3])
-			{
-				SetCheckResult(3);
-			}
-			else if (possible[4])
-			{
-				SetCheckResult(4);
-			}
-			else
-			{
-				SetCheckResult(1);
-			}
-		}
+    for (let vCount = fixedCount + 1; vCount <= 4; vCount++) {
+        c = fixedCount;
+        let used = fixedCount;
+        for (let i = 0; i < 6; i++) {
+            vFlag[i] = (ivs[i] === 31);
+        }
+        // 普通に個体値生成をやってみる
+        for (let i = 0; i < 6; i++) {
+            if (ivs[i] !== 31) {
+                used++;
+                let vPos = ivs[i] % 8;
+                if (vPos < 6 && vFlag[vPos] === false) {
+                    vFlag[vPos] = true;
+                    c++;
+                    if (c === vCount) // 遺伝終わり
+                    {
+                        // 未知の部分が連続してneedNumber以上
+                        if ((6 - vCount) - (6 - used) + (6 - fixedCount) >= needNumber) {
+                            possible[vCount] = true;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if (possible[3] && possible[4]) {
+        return SetCheckResult(7);
+    }
+    else if (possible[3]) {
+        return SetCheckResult(3);
+    }
+    else if (possible[4]) {
+        return SetCheckResult(4);
+    }
+    else {
+        return SetCheckResult(1);
+    }
+}
--- xoroshiro.js
+++ xoroshiro.js
@@ -1,13 +1,13 @@
 class Xoroshiro {
     constructor(seed) {
         // if(seed.toString()[1] !="x") seed = "0x"+ seed
-        this.s0 = BigInt.asUintN(64, BigInt(seed))
+        this.s0 = BigInt.asUintN(64, BigInt(seed));
         this.s1 = BigInt.asUintN(64, 0x82a2b175229d6a5bn)
         // console.log(this.s0,this.s1)
     }
 
     getRaw(index, mask) {
-        mask = BigInt.asUintN(64, BigInt(mask))
+        mask = BigInt.asUintN(64, BigInt(mask));
         if (index === 0) {
             return this.s0 & mask
         }
@@ -16,23 +16,23 @@ class Xoroshiro {
 
     rotateLeft(val, amount) {
         // val = BigInt.asUintN(64,val)
-        amount = BigInt.asUintN(64, BigInt(amount))
-        let left = BigInt.asUintN(64, val << amount)
-        let tmp = (BigInt(64) - amount)
-        let right = BigInt.asUintN(64, val >> tmp)
+        amount = BigInt.asUintN(64, BigInt(amount));
+        let left = BigInt.asUintN(64, val << amount);
+        let tmp = (BigInt(64) - amount);
+        let right = BigInt.asUintN(64, val >> tmp);
         //  console.log(left,right)
         return left | right
     }
 
     next(mask) {
         //if(mask.toString()[1] != "x") mask = parseInt(mask,16)
-        mask = BigInt.asUintN(32, BigInt(mask))
-        let val = (this.s0 + this.s1) & mask
-        this.s1 = BigInt.asUintN(64, this.s0 ^ this.s1)
+        mask = BigInt.asUintN(32, BigInt(mask));
+        let val = (this.s0 + this.s1) & mask;
+        this.s1 = BigInt.asUintN(64, this.s0 ^ this.s1);
         //console.log("s1" , this.s1)
-        this.s0 = this.rotateLeft(this.s0, 24) ^ this.s1 ^ BigInt.asUintN(64, (this.s1 << BigInt(16)))
+        this.s0 = this.rotateLeft(this.s0, 24) ^ this.s1 ^ BigInt.asUintN(64, (this.s1 << BigInt(16)));
         //  console.log("s01", this.s0)
-        this.s1 = BigInt.asUintN(64, this.rotateLeft(this.s1, 37))
+        this.s1 = BigInt.asUintN(64, this.rotateLeft(this.s1, 37));
         //   console.log("s0s1",this.s0,this.s1)
         return Number(val)
     }
@@ -41,36 +41,36 @@ class Xoroshiro {
 
 class ListGen {
     constructor(denSeed, maxCount, vCount, isShinycheck, isNogender, isHA, isShowseed) {
-        this.seed = typeof this.seed === 'bigint'? seed : BigInt(denSeed)
-        this.maxCount = maxCount || 100
-        this.vCount = vCount
-        this.isShinycheck = isShinycheck || false
-        this.isNogender = isNogender || false
-        this.isHA = isHA || true
+        this.seed = typeof this.seed === 'bigint' ? seed : BigInt(denSeed);
+        this.maxCount = maxCount || 100;
+        this.vCount = vCount;
+        this.isShinycheck = isShinycheck || false;
+        this.isNogender = isNogender || false;
+        this.isHA = isHA || true;
         this.isShowseed = isShowseed || false
     }
 
     generate() {
 
-        let ec, otid, pid = 0
-        let fixedIndex
-        let ability
-        let gender
-        let nature
-        let ivs = []
-        let results = []
+        let ec, otid, pid = 0;
+        let fixedIndex;
+        let ability;
+        let gender;
+        let nature;
+        let ivs = [];
+        let results = [];
         for (let frame = 0; frame < this.maxCount; frame++) {
-            let xs = new Xoroshiro(this.seed)
-            ec = Number(xs.next(BigInt.asUintN(32,0xFFFFFFFFn)))
-            otid = Number(xs.next(BigInt.asUintN(32,0xFFFFFFFFn)))
-            pid = Number(xs.next(BigInt.asUintN(32,0xFFFFFFFFn)))
+            let xs = new Xoroshiro(this.seed);
+            ec = Number(xs.next(BigInt.asUintN(32, 0xFFFFFFFFn)));
+            otid = Number(xs.next(BigInt.asUintN(32, 0xFFFFFFFFn)));
+            pid = Number(xs.next(BigInt.asUintN(32, 0xFFFFFFFFn)));
             //console.log(ec)
 
-            let isShiny = ((((otid ^ (otid >> 16)) >> 4) & 0xFFF) == (((pid ^ (pid >> 16)) >> 4) & 0xFFF));
-            let isSquare = (((otid ^ (otid >> 16)) & 0xFFFF) == ((pid ^ (pid >> 16)) & 0xFFFF));
+            let isShiny = ((((otid ^ (otid >> 16)) >> 4) & 0xFFF) === (((pid ^ (pid >> 16)) >> 4) & 0xFFF));
+            let isSquare = (((otid ^ (otid >> 16)) & 0xFFFF) === ((pid ^ (pid >> 16)) & 0xFFFF));
 
             if (this.isShinycheck && !isShiny) {
-                this.seed = this.seed + 0x82a2b175229d6a5bn
+                this.seed = this.seed + 0x82a2b175229d6a5bn;
                 continue;
             }
 
@@ -121,10 +121,10 @@ class ListGen {
                 ivs: Array.from(ivs),
                 isShiny: isSquare ? "square" : isShiny ? "star" : "no",
                 nature: nature,
-                ability: ability === 2 ? "HA" : ability+1,
+                ability: ability === 2 ? "HA" : ability + 1,
                 gender: gender,
-            }
-            results.push(final)
+            };
+            results.push(final);
             this.seed = BigInt.asUintN(64, this.seed + 0x82a2b175229d6a5bn);
         }
         return results
