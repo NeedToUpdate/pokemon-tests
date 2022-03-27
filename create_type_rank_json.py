e8c8e4cf558b12cf3756adcdd5e935ffdacdcbe6
Artem <updatethatpl0x@gmail.com>
2022-03-27 16:38:28 +0800

    added single types json

--- create_type_rank_json.py
+++ create_type_rank_json.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup as soup
 import json
+
+
 all_types = ['Rock', 'Steel', 'Bug', 'Fire', 'Normal', 'Electric', 'Poison', 'Fighting', 'Fairy', 'Psychic', 'Flying', 'Ice', 'Ground', 'Dark', 'Grass', 'Water', 'Ghost', 'Dragon']
 all_types = [x.lower() for x in all_types]
 type_json = "{\"Bug\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":1,\"Fairy\":1,\"Fighting\":0.5,\"Fire\":2,\"Flying\":2,\"Ghost\":1,\"Grass\":0.5,\"Ground\":0.5,\"Ice\":1,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":2,\"Steel\":1,\"Water\":1},\"Dark\":{\"Bug\":2,\"Dark\":0.5,\"Dragon\":1,\"Electric\":1,\"Fairy\":2,\"Fighting\":2,\"Fire\":1,\"Flying\":1,\"Ghost\":0.5,\"Grass\":1,\"Ground\":1,\"Ice\":1,\"Normal\":1,\"Poison\":1,\"Psychic\":0,\"Rock\":1,\"Steel\":1,\"Water\":1},\"Dragon\":{\"Bug\":1,\"Dark\":1,\"Dragon\":2,\"Electric\":0.5,\"Fairy\":2,\"Fighting\":1,\"Fire\":0.5,\"Flying\":1,\"Ghost\":1,\"Grass\":0.5,\"Ground\":1,\"Ice\":2,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":1,\"Steel\":1,\"Water\":0.5},\"Electric\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":0.5,\"Fairy\":1,\"Fighting\":1,\"Fire\":1,\"Flying\":0.5,\"Ghost\":1,\"Grass\":1,\"Ground\":2,\"Ice\":1,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":1,\"Steel\":0.5,\"Water\":1},\"Fairy\":{\"Bug\":0.5,\"Dark\":0.5,\"Dragon\":0,\"Electric\":1,\"Fairy\":1,\"Fighting\":0.5,\"Fire\":1,\"Flying\":1,\"Ghost\":1,\"Grass\":1,\"Ground\":1,\"Ice\":1,\"Normal\":1,\"Poison\":2,\"Psychic\":1,\"Rock\":1,\"Steel\":2,\"Water\":1},\"Fighting\":{\"Bug\":0.5,\"Dark\":0.5,\"Dragon\":1,\"Electric\":1,\"Fairy\":2,\"Fighting\":1,\"Fire\":1,\"Flying\":2,\"Ghost\":1,\"Grass\":1,\"Ground\":1,\"Ice\":1,\"Normal\":1,\"Poison\":1,\"Psychic\":2,\"Rock\":0.5,\"Steel\":1,\"Water\":1},\"Fire\":{\"Bug\":0.5,\"Dark\":1,\"Dragon\":1,\"Electric\":1,\"Fairy\":0.5,\"Fighting\":1,\"Fire\":0.5,\"Flying\":1,\"Ghost\":1,\"Grass\":0.5,\"Ground\":2,\"Ice\":0.5,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":2,\"Steel\":0.5,\"Water\":2},\"Flying\":{\"Bug\":0.5,\"Dark\":1,\"Dragon\":1,\"Electric\":2,\"Fairy\":1,\"Fighting\":0.5,\"Fire\":1,\"Flying\":1,\"Ghost\":1,\"Grass\":0.5,\"Ground\":0,\"Ice\":2,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":2,\"Steel\":1,\"Water\":1},\"Ghost\":{\"Bug\":0.5,\"Dark\":2,\"Dragon\":1,\"Electric\":1,\"Fairy\":1,\"Fighting\":0,\"Fire\":1,\"Flying\":1,\"Ghost\":2,\"Grass\":1,\"Ground\":1,\"Ice\":1,\"Normal\":0,\"Poison\":0.5,\"Psychic\":1,\"Rock\":1,\"Steel\":1,\"Water\":1},\"Grass\":{\"Bug\":2,\"Dark\":1,\"Dragon\":1,\"Electric\":0.5,\"Fairy\":1,\"Fighting\":1,\"Fire\":2,\"Flying\":2,\"Ghost\":1,\"Grass\":0.5,\"Ground\":0.5,\"Ice\":2,\"Normal\":1,\"Poison\":2,\"Psychic\":1,\"Rock\":1,\"Steel\":1,\"Water\":0.5},\"Ground\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":0,\"Fairy\":1,\"Fighting\":1,\"Fire\":1,\"Flying\":1,\"Ghost\":1,\"Grass\":2,\"Ground\":1,\"Ice\":2,\"Normal\":1,\"Poison\":0.5,\"Psychic\":1,\"Rock\":0.5,\"Steel\":1,\"Water\":2},\"Ice\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":1,\"Fairy\":1,\"Fighting\":2,\"Fire\":2,\"Flying\":1,\"Ghost\":1,\"Grass\":1,\"Ground\":1,\"Ice\":0.5,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":2,\"Steel\":2,\"Water\":1},\"Normal\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":1,\"Fairy\":1,\"Fighting\":2,\"Fire\":1,\"Flying\":1,\"Ghost\":0,\"Grass\":1,\"Ground\":1,\"Ice\":1,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":1,\"Steel\":1,\"Water\":1},\"Poison\":{\"Bug\":0.5,\"Dark\":1,\"Dragon\":1,\"Electric\":1,\"Fairy\":0.5,\"Fighting\":0.5,\"Fire\":1,\"Flying\":1,\"Ghost\":1,\"Grass\":0.5,\"Ground\":2,\"Ice\":1,\"Normal\":1,\"Poison\":0.5,\"Psychic\":2,\"Rock\":1,\"Steel\":1,\"Water\":1},\"Psychic\":{\"Bug\":2,\"Dark\":2,\"Dragon\":1,\"Electric\":1,\"Fairy\":1,\"Fighting\":0.5,\"Fire\":1,\"Flying\":1,\"Ghost\":2,\"Grass\":1,\"Ground\":1,\"Ice\":1,\"Normal\":1,\"Poison\":1,\"Psychic\":0.5,\"Rock\":1,\"Steel\":1,\"Water\":1},\"Rock\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":1,\"Fairy\":1,\"Fighting\":2,\"Fire\":0.5,\"Flying\":0.5,\"Ghost\":1,\"Grass\":2,\"Ground\":2,\"Ice\":1,\"Normal\":0.5,\"Poison\":0.5,\"Psychic\":1,\"Rock\":1,\"Steel\":2,\"Water\":2},\"Steel\":{\"Bug\":0.5,\"Dark\":1,\"Dragon\":0.5,\"Electric\":1,\"Fairy\":0.5,\"Fighting\":2,\"Fire\":2,\"Flying\":0.5,\"Ghost\":1,\"Grass\":0.5,\"Ground\":2,\"Ice\":0.5,\"Normal\":0.5,\"Poison\":0,\"Psychic\":0.5,\"Rock\":0.5,\"Steel\":0.5,\"Water\":1},\"Water\":{\"Bug\":1,\"Dark\":1,\"Dragon\":1,\"Electric\":2,\"Fairy\":1,\"Fighting\":1,\"Fire\":0.5,\"Flying\":1,\"Ghost\":1,\"Grass\":2,\"Ground\":1,\"Ice\":0.5,\"Normal\":1,\"Poison\":1,\"Psychic\":1,\"Rock\":1,\"Steel\":0.5,\"Water\":0.5}}"
-all_types_data = json.loads(type_json)
+all_types_data = json.loads(type_json.lower())
 
 def damageFrom(from_types, to_types):
     def _damageFrom(from_type, to_type):
@@ -79,8 +81,8 @@ def create_type_rank_dict(INCLUDE_SINGLE_TYPES = False):
                         all_stats[name]["does_damage_to"][t3] = {}
                     all_stats[name]["does_damage_to"][t3][t4] = {t1: t1dmg}
                     if t2 != 'none':
-                        all_stats[name]["does_damage_to"][t3][t4] = {t1: t1dmg, t2:t2dmg, 'sum':t1dmg+t2dmg, 'product': t1dmg*t2dmg }
                         t2dmg = damageFrom(t2, [t3,t4] if t4!= 'none' else t3)
+                        all_stats[name]["does_damage_to"][t3][t4] = {t1: t1dmg, t2:t2dmg, 'sum':t1dmg+t2dmg, 'product': t1dmg*t2dmg }
                         all_stats[name]["offensive_total"] += t2dmg
                         if t4 not in all_stats[name]["does_damage_to"]:
                             all_stats[name]["does_damage_to"][t4] = {}
@@ -91,8 +93,8 @@ def create_type_rank_dict(INCLUDE_SINGLE_TYPES = False):
                         all_stats[name]["takes_damage_from"][t3] = {}
                     all_stats[name]["takes_damage_from"][t3][t4] = {t3: t3def}
                     if t4 != 'none':
-                        all_stats[name]["takes_damage_from"][t3][t4] = {t3: t3def, t4:t4def, 'sum':t3def+t4def, 'product': t3def*t4def }
                         t4def = damageFrom(t4, [t1,t2] if t2!= 'none' else t1)
+                        all_stats[name]["takes_damage_from"][t3][t4] = {t3: t3def, t4:t4def, 'sum':t3def+t4def, 'product': t3def*t4def }
                         all_stats[name]["defensive_total"] += t4def
                         if t4 not in all_stats[name]["takes_damage_from"]:
                             all_stats[name]["takes_damage_from"][t4] = {}
@@ -113,11 +115,31 @@ def create_type_rank_dict(INCLUDE_SINGLE_TYPES = False):
     best_def_score = sorted(defensiveness, key=lambda item: item[1], reverse=True)[0][1]
     defensiveness = [(key, round((x/best_def_score)*best_off_score,2)) for (key,x) in defensiveness]
 
-    print(dict(sorted(offensiveness, key=lambda item: item[1], reverse=True)))
-    print(dict(sorted(defensiveness, key=lambda item: item[1], reverse=True)))
-
+    off = sorted(offensiveness, key=lambda item: item[1], reverse=True)
+    print('top 10 offensive: ')
+    for item in off[:10]:
+        print(item)
+    print('worst 10 offensive: ')
+    for item in off[-10:]:
+        print(item)
+    deff = sorted(defensiveness, key=lambda item: item[1], reverse=True)
+    print('top 10 defensive: ')
+    for item in deff[:10]:
+        print(item)
+    print('worst 10 defensive: ')
+    for item in deff[-10:]:
+        print(item)
     final_scores = [(key, round(score + dict(defensiveness)[key],2)) for (key,score) in offensiveness]
-    print(dict(sorted(final_scores, key=lambda item: item[1], reverse=True)))
+    final = sorted(final_scores, key=lambda item: item[1], reverse=True)
+
+    print('top 10 final score: ')
+    for item in final[:10]:
+        print(item)
+    print('worst 10 final score: ')
+    for item in final[-10:]:
+        print(item)
+
+
     for i, item in enumerate(sorted(offensiveness, key=lambda item: item[1], reverse=True)):
         all_stats[item[0]]['offensive_score'] = item[1]
         all_stats[item[0]]['offensive_score_placement'] = i  +1
@@ -148,5 +170,7 @@ def create_type_rank_dict(INCLUDE_SINGLE_TYPES = False):
     return final_json
 
 def save_json(final_json):
-    with open('./all_types.json','w') as f:
+    with open('./all_types_with_singles.json','w') as f:
         f.write(json.dumps(final_json))
+
+save_json(create_type_rank_dict(True))
